1c35a45ab6d541ab5b653e79434cb86106d6eaeb
Rajasekhar.Yekula <65954758+RajasekharYekula@users.noreply.github.com>
2022-12-02 10:46:12 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const app = require("./app");
+const mongoose = require("mongoose");
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION, APP SHUTTING NOW!!");
+  console.log(err.message, err.name);
+  process.exit(1);
+});
+
+//const DB = "ENTER YOUR CONNECTION STRING HERE";
+
+// mongoose
+//   .connect(DB, {
+//     useCreateIndex: true,
+//     useFindAndModify: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     autoIndex: true,
+//   })
+//   .then(() => {
+//     console.log("DB connected successfully");
+//   });
+
+const port = 3000;
+
+const server = app.listen(port, () => {
+  console.log("Server is up listening on port:3000");
+  console.log("DB connected successfully");
+});
